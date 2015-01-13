f04f623b0f3e4b182de9b0c6f5ce4bfbcd656e28
Mgamerz <mgamerz2@gmail.com>
2015-01-13 01:38:24 -0700

    commit

--- src/com/me3tweaks/modmanager/ModManager.java
+++ src/com/me3tweaks/modmanager/ModManager.java
@@ -32,7 +32,7 @@ import org.w3c.dom.Document;
 public class ModManager {
 	
 	public static final String VERSION = "3.0";
-	public static long BUILD_NUMBER = 29L;
+	public static long BUILD_NUMBER = 30L;
 	public static final String BUILD_DATE = "1/12/2015";
 	public static DebugLogger debugLogger;
 	public static boolean IS_DEBUG = true;
