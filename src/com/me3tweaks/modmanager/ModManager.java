b3cdf14d20cfd30ff18736d9555a2b84b735ea46
Mgamerz <mgamerz2@gmail.com>
2015-07-14 09:08:33 -0600

    BUILD 39: Fixed auto updater not starting compiler

--- src/com/me3tweaks/modmanager/ModManager.java
+++ src/com/me3tweaks/modmanager/ModManager.java
@@ -32,8 +32,8 @@ import org.w3c.dom.Document;
 
 public class ModManager {
 
-	public static final String VERSION = "3.1 RC2.02";
-	public static long BUILD_NUMBER = 36L;
+	public static final String VERSION = "3.1 RC2.03";
+	public static long BUILD_NUMBER = 39L;
 
 	public static final String BUILD_DATE = "7/13/2015";
 	public static DebugLogger debugLogger;
