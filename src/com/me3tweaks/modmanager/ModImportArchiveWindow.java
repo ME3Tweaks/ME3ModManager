444bb3b7eac4f47f9909496e72c9c22993932df9
Mgamerz <mgamerz2@gmail.com>
2016-02-22 18:20:28 -0700

    Added 7z/RAR/ZIP mod importer. No more need to download 7zip!

--- src/com/me3tweaks/modmanager/ModImportArchiveWindow.java
+++ src/com/me3tweaks/modmanager/ModImportArchiveWindow.java
@@ -1,46 +1,53 @@
 package com.me3tweaks.modmanager;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-import com.me3tweaks.modmanager.objects.ProcessResult;
+import com.me3tweaks.modmanager.objects.CompressedMod;
+import com.me3tweaks.modmanager.objects.ThreadCommand;
 import com.me3tweaks.modmanager.ui.HintTextFieldUI;
-
-import net.sf.sevenzipjbinding.IInArchive;
-import net.sf.sevenzipjbinding.SevenZip;
-import net.sf.sevenzipjbinding.SevenZipException;
-import net.sf.sevenzipjbinding.SevenZipNativeInitializationException;
-import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream;
-import net.sf.sevenzipjbinding.simple.ISimpleInArchive;
-import net.sf.sevenzipjbinding.simple.ISimpleInArchiveItem;
+import com.me3tweaks.modmanager.ui.JCheckBoxList;
+import com.me3tweaks.modmanager.utilities.SevenZipCompressedModInspector;
 
 public class ModImportArchiveWindow extends JFrame {
 
-	public static void main(String[] args) {
-		ModManager.loadLogger();
-		ModManager.logging = true;
-		new ModImportArchiveWindow();
-	}
+	private JCheckBoxList compressedModList;
+	private DefaultListModel<JCheckBox> compressedModModel;
+	HashMap<JCheckBox, CompressedMod> checkMap = new HashMap<JCheckBox, CompressedMod>(); //crazy, I know...
+	private JButton importButton;
+	private JProgressBar progressBar;
+	private JPanel leftsidePanel;
 
 	public ModImportArchiveWindow() {
 		setupWindow();
@@ -51,90 +58,80 @@ public class ModImportArchiveWindow extends JFrame {
 	private void setupWindow() {
 		setTitle("Import compressed mods into Mod Manager");
 		setIconImages(ModManager.ICONS);
+		setPreferredSize(new Dimension(650, 400));
+
+		JTextArea descriptionArea = new JTextArea();
+		descriptionArea.setLineWrap(true);
+		descriptionArea.setWrapStyleWord(true);
+		descriptionArea.setEditable(false);
+		descriptionArea.setText("Select an archive file to scan contents for Mod Manager mods.");
 
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		GridBagConstraints c = new GridBagConstraints();
 
-		//Decompile Panel
-		JPanel decompilePanel = new JPanel(new GridBagLayout());
-		decompilePanel.setBorder(new TitledBorder(new EtchedBorder(), "Select archive to import"));
-		JTextField dInputField = new JTextField(55);
-		dInputField.setUI(new HintTextFieldUI("Select an archive (.7z, .zip, .rar)"));
-		JButton dBrowse = new JButton("Browse...");
-		dBrowse.setPreferredSize(new Dimension(100, 19));
-		JButton dCompile = new JButton("Import");
-		dCompile.setEnabled(false);
-		dCompile.setPreferredSize(new Dimension(100, 23));
-		JLabel dStatus = new JLabel(" ");
+		//Browse Panel
+		leftsidePanel = new JPanel(new BorderLayout());
+		leftsidePanel.setBorder(new TitledBorder(new EtchedBorder(), "Select archive to import"));
 
-		c = new GridBagConstraints();
-		c.gridx = 0;
-		decompilePanel.add(dInputField, c);
+		JPanel browsePanel = new JPanel(new BorderLayout());
+		JTextField archivePathField = new JTextField(55);
+		archivePathField.setUI(new HintTextFieldUI("Select an archive (.7z, .zip, .rar)"));
+		archivePathField.setEnabled(false);
+		JButton browseButton = new JButton("Browse...");
+		browseButton.setPreferredSize(new Dimension(100, 19));
+		browsePanel.add(archivePathField, BorderLayout.CENTER);
+		browsePanel.add(browseButton, BorderLayout.EAST);
 
-		c.gridx = 1;
-		decompilePanel.add(dBrowse, c);
+		compressedModList = new JCheckBoxList();
+		compressedModModel = new DefaultListModel<>();
+		compressedModList.setModel(compressedModModel);
+		compressedModList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		compressedModList.addListSelectionListener(new ListSelectionListener() {
 
-		c.gridx = 0;
-		c.gridy = 1;
-		c.anchor = GridBagConstraints.WEST;
-		decompilePanel.add(dStatus, c);
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				System.out.println("Item is changing");
+				if (e.getValueIsAdjusting() == false) {
+					System.out.println("Item not adjusting");
+					if (compressedModList.getSelectedIndex() == -1) {
+						descriptionArea.setText("Select a mod on the left to view its description.");
+					} else {
+						descriptionArea.setText(checkMap.get(compressedModModel.get(compressedModList.getSelectedIndex())).getModDescription());
+					}
+				}
+			}
+		});
 
-		c.gridx = 1;
-		c.gridy = 1;
-		c.anchor = GridBagConstraints.EAST;
-		decompilePanel.add(dCompile, c);
+		importButton = new JButton("Import Selected");
+		importButton.setEnabled(false);
+		importButton.setPreferredSize(new Dimension(100, 23));
+		progressBar = new JProgressBar();
 
-		//Compile Panel
-		JPanel compilePanel = new JPanel(new GridBagLayout());
-		compilePanel.setBorder(new TitledBorder(new EtchedBorder(), "Unused panel"));
-		JTextField cInputField = new JTextField(55);
-		cInputField.setUI(new HintTextFieldUI("Select a .xml manifest file"));
-		JButton cBrowse = new JButton("Browse...");
-		cBrowse.setPreferredSize(new Dimension(100, 19));
-		JButton cCompile = new JButton("Compile");
-		cCompile.setPreferredSize(new Dimension(100, 23));
-		cCompile.setEnabled(false);
-		JLabel cStatus = new JLabel("Manifest file is the .xml file with the same name as the file that was originally decompiled");
+		JScrollPane importScroller = new JScrollPane(compressedModList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		importScroller.setEnabled(false);
 
-		c = new GridBagConstraints();
-		c.gridx = 0;
-		compilePanel.add(cInputField, c);
+		leftsidePanel.add(browsePanel, BorderLayout.NORTH);
+		leftsidePanel.add(importScroller, BorderLayout.CENTER);
+		leftsidePanel.add(importButton, BorderLayout.SOUTH);
 
-		c.gridx = 1;
-		compilePanel.add(cBrowse, c);
+		//RIGHT SIDE
+		JScrollPane descScroller = new JScrollPane(descriptionArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-		c.gridx = 0;
-		c.gridy = 1;
-		c.anchor = GridBagConstraints.WEST;
-		compilePanel.add(cStatus, c);
-
-		c.gridx = 1;
-		c.gridy = 1;
-		c.anchor = GridBagConstraints.EAST;
-		compilePanel.add(cCompile, c);
-
-		JLabel label = new JLabel("<html>You can compile or decompile Coalesced files using this tool, for both basegame and DLC.</html>");
-		label.setAlignmentX(Component.LEFT_ALIGNMENT);
-		dStatus.setAlignmentX(Component.LEFT_ALIGNMENT);
-		cStatus.setAlignmentX(Component.LEFT_ALIGNMENT);
-		decompilePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-		compilePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-		panel.add(label);
-		panel.add(decompilePanel);
-		panel.add(compilePanel);
-
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftsidePanel, descScroller);
+		leftsidePanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panel.add(splitPane);
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		add(panel);
 		pack();
+		splitPane.setDividerLocation(0.5);
 
-		dBrowse.addActionListener(new ActionListener() {
+		browseButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser binChooser = new JFileChooser();
-				File tryDir = new File(dInputField.getText());
+				File tryDir = new File(archivePathField.getText());
 				if (tryDir.exists() && tryDir.isFile()) {
 					binChooser.setCurrentDirectory(tryDir.getParentFile());
 				} else {
@@ -149,131 +146,117 @@ public class ModImportArchiveWindow extends JFrame {
 				binChooser.setFileFilter(new FileNameExtensionFilter("Compressed Archive Files (.7z/.rar/.zip)", "7z", "rar", "zip"));
 
 				if (binChooser.showOpenDialog(ModImportArchiveWindow.this) == JFileChooser.APPROVE_OPTION) {
+					compressedModModel.clear();
 					String chosenFile = binChooser.getSelectedFile().getAbsolutePath();
-					if (chosenFile.toLowerCase().endsWith("PCConsoleTOC.bin".toLowerCase())) {
-						dStatus.setText("Selected PCConsoleTOC file, not a Coalesced file.");
-					} else {
-						dInputField.setText(chosenFile);
-						dStatus.setText(" ");
-						dCompile.setEnabled(true);
-						//test
-						try {
-							SevenZip.initSevenZipFromPlatformJAR();
-							System.out.println("7-Zip-JBinding library was initialized");
-							RandomAccessFile randomAccessFile = new RandomAccessFile(chosenFile, "r");
-
-							IInArchive inArchive = SevenZip.openInArchive(null, // Choose format automatically
-						            new RandomAccessFileInStream(randomAccessFile));
-							ISimpleInArchive simpleInArchive = inArchive.getSimpleInterface();
-							for (ISimpleInArchiveItem item : simpleInArchive.getArchiveItems()) {
-								System.out.println(String.format("%9s | %9s | %s", // 
-				                        item.getSize(), 
-				                        item.getPackedSize(), 
-				                        item.getPath()));
-				            }
-							
-							
-							System.out.println(inArchive.getNumberOfItems());
-							inArchive.close();
-							
-						} catch (SevenZipNativeInitializationException ex) {
-							ex.printStackTrace();
-						} catch (SevenZipException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						} catch (FileNotFoundException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
+					archivePathField.setText(chosenFile);
+					importButton.setEnabled(true);
+					ArrayList<CompressedMod> compressedMods = SevenZipCompressedModInspector.getCompressedModsInArchive(chosenFile);
+					for (CompressedMod cm : compressedMods) {
+						JCheckBox importBox = new JCheckBox(cm.getModName());
+						importBox.setSelected(true);
+						checkMap.put(importBox, cm);
+						compressedModModel.addElement(importBox);
 					}
 				}
 			}
 		});
 
-		cBrowse.addActionListener(new ActionListener() {
+		importButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JFileChooser xmlChooser = new JFileChooser();
-				File tryFile = new File(cInputField.getText());
-				if (tryFile.exists() && tryFile.isFile()) {
-					xmlChooser.setCurrentDirectory(tryFile.getParentFile());
-				} else {
-					xmlChooser.setCurrentDirectory(new File("."));
+				ArrayList<CompressedMod> modsToImport = new ArrayList<>();
+				for (Map.Entry<JCheckBox, CompressedMod> entry : checkMap.entrySet()) {
+					JCheckBox key = entry.getKey();
+					CompressedMod value = entry.getValue();
+
+					if (key.isSelected()) {
+						modsToImport.add(value);
+					}
 				}
-				xmlChooser.setDialogTitle("Select .xml manifest to compile");
-				//binChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-				//
-				// disable the "All files" option.
-				//
-				xmlChooser.setAcceptAllFileFilterUsed(false);
-				xmlChooser.setFileFilter(new FileNameExtensionFilter("Coalesced Manifest File (.xml)", "xml"));
-
-				if (xmlChooser.showOpenDialog(ModImportArchiveWindow.this) == JFileChooser.APPROVE_OPTION) {
-					String chosenFile = xmlChooser.getSelectedFile().getAbsolutePath();
-					cInputField.setText(chosenFile);
-					cCompile.setEnabled(true);
-					
-				} else {
-
+				if (modsToImport.size() > 0) {
+					ImportWorker worker = new ImportWorker(archivePathField.getText(), modsToImport);
+					importButton.setText("Importing");
+					importButton.setEnabled(false);
+					leftsidePanel.remove(importButton);
+					leftsidePanel.add(progressBar, BorderLayout.SOUTH);
+					pack();
+					worker.execute();
 				}
 			}
 		});
+	}
 
-		dCompile.addActionListener(new ActionListener() {
+	class ScanWorker extends SwingWorker<Void, Void> {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (new File(dInputField.getText()).exists()) {
-					String compilerPath = ModManager.getTankMasterCompilerDir() + "MassEffect3.Coalesce.exe";
+		@Override
+		protected Void doInBackground() throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
 
-					ArrayList<String> commandBuilder = new ArrayList<String>();
-					commandBuilder.add(compilerPath);
-					commandBuilder.add(dInputField.getText());
-					//System.out.println("Building command");
-					String[] command = commandBuilder.toArray(new String[commandBuilder.size()]);
-					//Debug stuff
-					StringBuilder sb = new StringBuilder();
-					for (String arg : command) {
-						sb.append(arg + " ");
-					}
-					ProcessBuilder decompileProcessBuilder = new ProcessBuilder(command);
-					decompileProcessBuilder.redirectErrorStream(true);
-					decompileProcessBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-					ProcessResult res = ModManager.runProcess(decompileProcessBuilder);
-					if (!res.hadError() && res.getReturnCode() == 0) {
-						dStatus.setText("Decompiled .bin file. Files are located in a folder of the same name.");
-					}
+	}
+
+	public class ImportWorker extends SwingWorker<Void, ThreadCommand> {
+
+		private ArrayList<CompressedMod> modsToImport;
+		private String archiveFilePath;
+
+		public ImportWorker(String archiveFilePath, ArrayList<CompressedMod> modsToImport) {
+			// TODO Auto-generated constructor stub
+			this.archiveFilePath = archiveFilePath;
+			this.modsToImport = modsToImport;
+		}
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			// TODO Auto-generated method stub
+			SevenZipCompressedModInspector.extractCompressedModsFromArchive(archiveFilePath, modsToImport, this);
+			return null;
+		}
+
+		protected void process(List<ThreadCommand> commands) {
+			for (ThreadCommand command : commands) {
+				switch (command.getCommand()) {
+				case "PROGRESS_UPDATE":
+					progressBar.setValue(Integer.parseInt((String) command.getMessage()));
+					break;
 				}
 			}
-		});
+		}
 
-		cCompile.addActionListener(new ActionListener() {
+		public void setProgressValue(int progress) {
+			publish(new ThreadCommand("PROGRESS_UPDATE", Integer.toString(progress)));
+		}
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (new File(cInputField.getText()).exists()) {
-					String compilerPath = ModManager.getTankMasterCompilerDir() + "MassEffect3.Coalesce.exe";
-
-					ArrayList<String> commandBuilder = new ArrayList<String>();
-					commandBuilder.add(compilerPath);
-					commandBuilder.add(cInputField.getText());
-					//System.out.println("Building command");
-					String[] command = commandBuilder.toArray(new String[commandBuilder.size()]);
-					//Debug stuff
-					StringBuilder sb = new StringBuilder();
-					for (String arg : command) {
-						sb.append(arg + " ");
-					}
-					ProcessBuilder compileProcessBuilder = new ProcessBuilder(command);
-					compileProcessBuilder.redirectErrorStream(true);
-					compileProcessBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-					ProcessResult res = ModManager.runProcess(compileProcessBuilder);
-					if (!res.hadError() && res.getReturnCode() == 0) {
-						cStatus.setText("Compiled .bin file. The new file is located in the same folder as the manifest.");
-					}
-				}
+		public void done() {
+			boolean error = false;
+			try {
+				get();
+			} catch (ExecutionException | InterruptedException e) {
+				error = true;
+				ModManager.debugLogger.writeErrorWithException("Error extracting mod archive:", e);
 			}
-		});
+
+			for (int i = 0; i < compressedModModel.size(); i++) {
+				JCheckBox box = compressedModModel.getElementAt(i);
+				box.setSelected(false);
+			}
+
+			importButton.setText("Import Finished");
+			leftsidePanel.remove(progressBar);
+			leftsidePanel.add(importButton, BorderLayout.SOUTH);
+			pack();
+			repaint();
+			if (!error) {
+				JOptionPane.showMessageDialog(ModImportArchiveWindow.this, "Mods have been imported.", "Import Successful", JOptionPane.INFORMATION_MESSAGE);
+				dispose();
+				new ModManagerWindow(false);
+			} else {
+				JOptionPane.showMessageDialog(ModImportArchiveWindow.this, "Error occured during mod import.\nSome mods may have successfully imported.\nReload Mod Manager from the actions menu\nto see new mods that may have imported.", "Import Unsuccessful", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+
 	}
 }
