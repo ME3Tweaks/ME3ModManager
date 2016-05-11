be613e05dcbd818d485ed3f2009da95d0ce321f7
Mgamerz <mgamerz2@gmail.com>
2016-05-10 22:43:15 -0600

    Log viewer now tails

--- src/com/me3tweaks/modmanager/utilities/DebugLogger.java
+++ src/com/me3tweaks/modmanager/utilities/DebugLogger.java
@@ -1,11 +1,14 @@
 package com.me3tweaks.modmanager.utilities;
 
+import java.awt.Toolkit;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -28,6 +31,7 @@ public class DebugLogger {
 	int currentMessages = 0;
 	public final static String LOGGING_FILENAME = "me3cmm_last_run_log.txt";
 	private boolean initialized = false;
+	private Timer timer;
 
 	public DebugLogger() {
 
@@ -59,6 +63,7 @@ public class DebugLogger {
 						Date date = new Date();
 						writeMessage("Logger shutting down. Time: " + dateFormat.format(date));
 						fw.close();
+						timer.cancel();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						System.out.println("Cannot close filewriter. Giving up.");
@@ -67,6 +72,9 @@ public class DebugLogger {
 				}
 			});
 			initialized = true;
+			//schedule flush every 5 seconds
+			timer = new Timer();
+			timer.scheduleAtFixedRate(new FlushTask(), 5000, 5000);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			logFile = null;
@@ -78,6 +86,14 @@ public class DebugLogger {
 
 	public boolean isInitialized() {
 		return initialized;
+	}
+
+	class FlushTask extends TimerTask {
+		public void run() {
+			System.out.println("periodic flush");
+			currentMessages = 100;
+			checkIfFlushNeeded();
+		}
 	}
 
 	public synchronized void writeMessage(String message) {
@@ -127,7 +143,7 @@ public class DebugLogger {
 			try {
 				System.err.println("[L:E]: " + message);
 				if (fw != null) {
-					fw.write(ERROR_PREFIX+": ");
+					fw.write(ERROR_PREFIX + ": ");
 					fw.write(message);
 					fw.write(System.getProperty("line.separator"));
 				}
