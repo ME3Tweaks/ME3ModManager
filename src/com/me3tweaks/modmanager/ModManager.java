cf064856150c2fb12ee0f0aa05af3183ba1a8ee4
Mgamerz <mgamerz2@gmail.com>
2017-01-17 18:18:43 -0700

    Mod Manager 4.4.3: Fix Compat Generator Charsets

--- src/com/me3tweaks/modmanager/ModManager.java
+++ src/com/me3tweaks/modmanager/ModManager.java
@@ -81,8 +81,8 @@ import com.sun.jna.win32.W32APIOptions;
 public class ModManager {
 
 	public static final String VERSION = "4.4.3";
-	public static long BUILD_NUMBER = 64L;
-	public static final String BUILD_DATE = "1/5/2017";
+	public static long BUILD_NUMBER = 65L;
+	public static final String BUILD_DATE = "1/17/2017";
 	public static DebugLogger debugLogger;
 	public static boolean IS_DEBUG = false;
 	public static final String SETTINGS_FILENAME = "me3cmm.ini";
