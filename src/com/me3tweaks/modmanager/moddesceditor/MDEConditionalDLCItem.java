1a5e0c26ed60e524a89f648838868afa890e6b48
Mgamerz <mgamerz2@gmail.com>
2018-01-04 22:43:51 -0700

    Add missing files

---
+++ src/com/me3tweaks/modmanager/moddesceditor/MDEConditionalDLCItem.java
@@ -0,0 +1,305 @@
+package com.me3tweaks.modmanager.moddesceditor;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import org.jdesktop.swingx.HorizontalLayout;
+import org.jdesktop.swingx.JXCollapsiblePane;
+
+import com.me3tweaks.modmanager.objects.AlternateCustomDLC;
+import com.me3tweaks.modmanager.objects.AlternateFile;
+import com.me3tweaks.modmanager.objects.Mod;
+import com.me3tweaks.modmanager.objects.ModJob;
+import com.me3tweaks.modmanager.objects.ModTypeConstants;
+import com.me3tweaks.modmanager.ui.SwingLink;
+import com.me3tweaks.modmanager.utilities.ResourceUtils;
+
+import javafx.application.Platform;
+import javafx.stage.FileChooser;
+
+public class MDEConditionalDLCItem {
+	private JXCollapsiblePane collapsablePanel;
+
+	public JXCollapsiblePane getPanel() {
+		return collapsablePanel;
+	}
+
+	public AlternateCustomDLC getAlternateDLC() {
+		return altdlc;
+	}
+
+	public JComboBox<String> getConditionBox() {
+		return conditionBox;
+	}
+
+	public static String[] getConditions() {
+		return conditions;
+	}
+
+	public static String[] getConditionsHuman() {
+		return conditionsHuman;
+	}
+
+	public JComboBox<String> getOperationBox() {
+		return operationBox;
+	}
+
+	public static String[] getOperations() {
+		return operations;
+	}
+
+	public JButton getMinusButton() {
+		return minusButton;
+	}
+
+	public JTextField getDescriptionField() {
+		return descriptionField;
+	}
+
+	public JTextField getUserReasonField() {
+		return userReasonField;
+	}
+
+	public void setUserReasonField(JTextField userReasonField) {
+		this.userReasonField = userReasonField;
+	}
+
+	private AlternateCustomDLC altdlc;
+
+	private JComboBox<String> conditionBox;
+	private static String[] conditions = { AlternateCustomDLC.CONDITION_DLC_NOT_PRESENT, AlternateCustomDLC.CONDITION_DLC_PRESENT, AlternateCustomDLC.CONDITION_ALL_DLC_PRESENT,
+			AlternateCustomDLC.CONDITION_ANY_DLC_NOT_PRESENT, AlternateCustomDLC.CONDITION_MANUAL };
+	private static String[] conditionsHuman = { "is not installed", "is installed", "are all installed", "any is not installed", "is selected by user" };
+
+	private JComboBox<String> operationBox;
+	private static String[] operations = { AlternateCustomDLC.OPERATION_ADD_CUSTOMDLC_JOB, AlternateCustomDLC.OPERATION_ADD_FILES_TO_CUSTOMDLC_FOLDER };
+	private static String[] operationsHuman = { "add additional custom DLC", "merge folder" };
+
+	private JButton minusButton;
+	private JTextField descriptionField, userReasonField;
+
+	private Mod mod;
+
+	private ModDescEditorWindow owningWindow;
+
+	private SwingLink srcLabel;
+
+	private SwingLink destLabel;
+	private static int itemSpacing = 5;
+
+	//private String altFile;
+	private String modFile;
+
+	public MDEConditionalDLCItem(ModDescEditorWindow owningWindow, AlternateCustomDLC af2) {
+		this.mod = owningWindow.getMod();
+		this.owningWindow = owningWindow;
+		boolean startCollapsed = false;
+		if (af2 == null) {
+			af2 = new AlternateCustomDLC();
+			startCollapsed = true;
+		}
+		this.altdlc = af2;
+		setupPanel();
+		collapsablePanel.setCollapsed(startCollapsed);
+	}
+
+	private void setupPanel() {
+		collapsablePanel = new JXCollapsiblePane();
+		JPanel panel = new JPanel(new HorizontalLayout());
+		collapsablePanel.add(panel);
+		minusButton = new JButton("-");
+		panel.add(minusButton);
+		minusButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				collapsablePanel.setCollapsed(true);
+				owningWindow.removeConditionalDLCItem(MDEConditionalDLCItem.this);
+			}
+		});
+
+		conditionBox = new JComboBox<String>(conditionsHuman);
+		operationBox = new JComboBox<String>(operationsHuman);
+		userReasonField = new JTextField(altdlc.getFriendlyName());
+		userReasonField.setToolTipText("User friendly name. If one is not entered, a automatically generated one is displayed instead.");
+		userReasonField.setColumns(30);
+		panel.add(userReasonField);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing * 2, 3)));
+		panel.add(new JSeparator(JSeparator.VERTICAL));
+
+		JLabel ifLabel = new JLabel("If");
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+		panel.add(ifLabel);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+		JTextField conditionalDLC = new JTextField(altdlc.getConditionalDLC(), 16);
+		panel.add(conditionalDLC);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+		panel.add(operationBox);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+		panel.add(conditionBox);
+
+		JLabel thenLabel = new JLabel("then");
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+		panel.add(thenLabel);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+
+		panel.add(operationBox);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+
+		JPanel rightSideVerticalPanel = new JPanel();
+		rightSideVerticalPanel.setLayout(new BoxLayout(rightSideVerticalPanel, BoxLayout.Y_AXIS));
+		rightSideVerticalPanel.add(Box.createVerticalGlue());
+		ActionListener srcChangeAction = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				Platform.runLater(() -> {
+					FileChooser fileChooser = new FileChooser();
+					fileChooser.setInitialDirectory(new File(mod.getModPath()));
+					//Set extension filter
+					FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("Mod File", "*.*");
+					fileChooser.getExtensionFilters().addAll(extFilterJPG);
+
+					//Show open file dialog
+					File file = fileChooser.showOpenDialog(null);
+					if (file != null) {
+						SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+								//Run on UI thread
+
+								//we need to verify file...
+								String relativePath = ResourceUtils.getRelativePath(file.getAbsolutePath(), mod.getModPath(), File.separator);
+								if (relativePath.contains("..")) {
+									//reject as out of mod folder.
+
+									JOptionPane.showMessageDialog(owningWindow, "This file is not in the mod folder.", "Invalid file", JOptionPane.ERROR_MESSAGE);
+								}
+
+								//File is valid
+								srcLabel.setText(relativePath);
+							}
+						});
+					}
+				});
+			}
+		};
+		srcLabel = new SwingLink(altdlc.getAltDLC(), "Click to change file", srcChangeAction);
+		rightSideVerticalPanel.add(srcLabel);
+
+		JLabel intoLabel = new JLabel("into");
+		//Component forSpace = Box.createRigidArea(new Dimension(itemSpacing, 3));
+		//panel.add(forSpace);
+		rightSideVerticalPanel.add(intoLabel);
+
+		ActionListener changeDestAction = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ModJob job = mod.getJobByModuleName(ModTypeConstants.CUSTOMDLC);
+				if (job != null) {
+					int index = operationBox.getSelectedIndex();
+					if (index > 0) { //if 0, add mode
+						index = MDEModFileChooser.OPTIONTYPE_SELECTONLY; //select only
+					} else {
+						index = MDEModFileChooser.OPTIONTYPE_ADDONLY;
+					}
+					MDEModFolderChooser fileChooser = new MDEModFolderChooser(owningWindow, modFile, index, job);
+					if (fileChooser.getSelectedFile() != null) {
+						modFile = fileChooser.getSelectedFile();
+						destLabel.setText(modFile);
+					}
+				}
+			}
+		};
+		modFile = altdlc.getDestDLC();
+		destLabel = new SwingLink(modFile, "Click to change file", changeDestAction);
+		//panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+		rightSideVerticalPanel.add(destLabel);
+		rightSideVerticalPanel.add(Box.createVerticalGlue());
+
+		panel.add(rightSideVerticalPanel);
+		panel.add(Box.createRigidArea(new Dimension(itemSpacing, 3)));
+
+		operationBox.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					int index = operationBox.getSelectedIndex();
+					switch (index) {
+					case 0: //add
+						destLabel.setVisible(false);
+						intoLabel.setVisible(false);
+						break;
+					case 1: //merge
+						destLabel.setVisible(true);
+						intoLabel.setVisible(true);
+						break;
+					}
+				}
+			}
+		});
+		
+		//hide into/dest for add
+		if (operationBox.getSelectedIndex() == 0) {
+			destLabel.setVisible(false);
+			intoLabel.setVisible(false);
+		}
+		
+		
+		if (altdlc.getCondition() != null) {
+			switch (altdlc.getCondition()) {
+			case AlternateCustomDLC.CONDITION_DLC_NOT_PRESENT:
+				conditionBox.setSelectedIndex(0);
+				break;
+			case AlternateCustomDLC.CONDITION_DLC_PRESENT:
+				conditionBox.setSelectedIndex(1);
+				break;
+			case AlternateCustomDLC.CONDITION_ALL_DLC_PRESENT:
+				conditionBox.setSelectedIndex(2);
+				break;
+			case AlternateCustomDLC.CONDITION_ANY_DLC_NOT_PRESENT:
+				conditionBox.setSelectedIndex(3);
+				break;
+			case AlternateCustomDLC.CONDITION_MANUAL:
+				conditionBox.setSelectedIndex(4);
+				break;
+			}
+		} else {
+			conditionBox.setSelectedIndex(0); //default
+		}
+
+		if (altdlc.getOperation() != null) {
+			switch (altdlc.getOperation()) {
+			case AlternateCustomDLC.OPERATION_ADD_CUSTOMDLC_JOB:
+				operationBox.setSelectedIndex(0);
+				break;
+			case AlternateCustomDLC.OPERATION_ADD_FILES_TO_CUSTOMDLC_FOLDER:
+				operationBox.setSelectedIndex(1);
+				break;
+			}
+		} else {
+			operationBox.setSelectedIndex(0);
+		}
+
+	}
+
+	public String getDLCDestination() {
+		return destLabel.getRawText();
+	}
+}
---
+++ src/com/me3tweaks/modmanager/objects/MetaCMM.java
@@ -0,0 +1,88 @@
+package com.me3tweaks.modmanager.objects;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+
+import com.me3tweaks.modmanager.ModManager;
+
+public class MetaCMM {
+
+	private String installationGUID = "";
+	private String installedByModManagerBuild;
+	private String modVersion;
+	private String modName;
+
+	public MetaCMM(File metacmmfile) {
+		if (metacmmfile.exists()) {
+			try (BufferedReader br = new BufferedReader(new FileReader(metacmmfile))) {
+				String line;
+				if ((line = br.readLine()) != null) {
+					//Name of installed mod, or name + version if old mod manager versions
+					modName = line;
+				}
+
+				if ((line = br.readLine()) != null) {
+					//Name of installed mod, or name + version if old mod manager versions
+					modVersion = line;
+				}
+
+				if ((line = br.readLine()) != null) {
+					//Name of installed mod, or name + version if old mod manager versions
+					installedByModManagerBuild = line;
+				}
+
+				if ((line = br.readLine()) != null) {
+					//Name of installed mod, or name + version if old mod manager versions
+					installationGUID = line;
+				}
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public String getInstallationGUID() {
+		return installationGUID;
+	}
+
+	public String getInstalledByModManagerBuild() {
+		return installedByModManagerBuild;
+	}
+
+	public String getModVersion() {
+		return modVersion;
+	}
+
+	public String getModName() {
+		return modName;
+	}
+
+	public String getDisplayString() {
+		return modName + (modVersion != null ? " " + modVersion : "");
+	}
+
+	public static void writeMetaCMMFile(Mod mod, String destination) {
+		try {
+			ModManager.debugLogger.writeMessage("[METACMM]Writing custom DLC metadata file: " + destination);
+			String metadatatext = mod.getModName();
+			metadatatext += "\n"+mod.getVersion();
+			metadatatext += "\n"+ModManager.BUILD_NUMBER;
+			metadatatext += "\n"+ModManager.getGUID();
+			
+			FileUtils.writeStringToFile(new File(destination), metadatatext, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			ModManager.debugLogger.writeErrorWithException("[METACMM]Couldn't write custom dlc metadata file:", e);
+		}
+	}
+}
