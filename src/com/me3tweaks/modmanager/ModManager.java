28186f3623239d6d41f2f92b5e4f176131e34477
Mgamerz <mgamerz2@gmail.com>
2018-02-09 17:16:21 -0700

    Enabled full sunset date of 32-bit mod manager on May 15 2018. Fixed biogame directory saving bug and backported biogame selection from 5.1.

--- src/com/me3tweaks/modmanager/ModManager.java
+++ src/com/me3tweaks/modmanager/ModManager.java
@@ -82,9 +82,8 @@ import javafx.embed.swing.JFXPanel;
 
 public class ModManager {
 	public static boolean IS_DEBUG = false;
-	public final static boolean FORCE_32BIT_MODE = false; //set to true to force it to think it is running 32-bit for (most things)
 
-	public static final String VERSION = "5.0.8";
+	public static final String VERSION = "5.0.9";
 	public static long BUILD_NUMBER = 83L;
 	public static final String BUILD_DATE = "01/23/2017";
 	public static final String SETTINGS_FILENAME = "me3cmm.ini";
--- src/com/me3tweaks/modmanager/utilities/ResourceUtils.java
+++ src/com/me3tweaks/modmanager/utilities/ResourceUtils.java
@@ -495,9 +495,7 @@ public class ResourceUtils {
 	 * @return true if 64bit, false otherwise
 	 */
 	public static boolean is64BitWindows() {
-		if (ModManager.FORCE_32BIT_MODE) {
-			return false;
-		}
+		if (true) return false;
 		if (System.getProperty("os.name").contains("Windows")) {
 			return (System.getenv("ProgramFiles(x86)") != null);
 		} else {
