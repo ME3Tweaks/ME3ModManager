678a1d4edd50146b9d64ec8e308094c85b46357c
Mgamerz <mgamerz2@gmail.com>
2019-09-28 16:59:17 -0600

    Disable debug mode

--- src/com/me3tweaks/modmanager/ModManager.java
+++ src/com/me3tweaks/modmanager/ModManager.java
@@ -36,7 +36,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.prefs.Preferences;
 
 public class ModManager {
-    public static boolean IS_DEBUG = true;
+    public static boolean IS_DEBUG = false;
     public static final String VERSION = "5.1.6";
     public static long BUILD_NUMBER = 93L;
     public static final String BUILD_DATE = "09/28/2019";
